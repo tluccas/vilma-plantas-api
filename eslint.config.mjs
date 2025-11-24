160ab17c49b33829c3310b3832b08e3cfbd6524e
Lucas Alves <lucas1901alves@gmail.com>
2025-11-23 22:11:37 -0300

    feat: add eslint & prettier configs

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import prettier from 'eslint-config-prettier';
+import jsonPlugin from 'eslint-plugin-json';
+import json from '@eslint/json';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended', prettier],
+    languageOptions: { globals: globals.node },
+    rules: {
+      semi: ['error', 'always'],
+    },
+  },
+  {
+    files: ['**/*.json'],
+    plugins: { json: jsonPlugin },
+    language: 'json/json',
+    extends: ['json/recommended'],
+  },
+]);
