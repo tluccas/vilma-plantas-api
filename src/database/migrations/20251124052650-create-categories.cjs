9e026e479098b682f79e65e15fe93d19f52263b8
Lucas Alves <lucas1901alves@gmail.com>
2025-11-24 02:31:46 -0300

    feat(migrate): add categories migrate

---
+++ src/database/migrations/20251124052650-create-categories.cjs
@@ -0,0 +1,34 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.createTable('categories', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.TEXT,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down (queryInterface) {
+    return queryInterface.dropTable('categories');
+  }
+};
