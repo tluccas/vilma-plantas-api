cf864dac77e4625f089d592b78515155b0d28385
Lucas Alves <lucas1901alves@gmail.com>
2025-11-26 02:05:39 -0300

    feat(auth): add auth controller - Methods: create & delete

---
+++ src/controllers/auth/AuthController.js
@@ -0,0 +1,48 @@
+import User from '../../database/models/User.js';
+import JWT from 'jsonwebtoken';
+import authConfig from '../../config/authConfig.js';
+
+class AuthController {
+  async create(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(404).json({ error: 'Usuário não encontrado' });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ error: 'Senha incorreta' });
+    }
+
+    const { id, name, role } = user;
+
+    const token =
+      ('token',
+      JWT.sign({ id, role }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }));
+
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 48 * 60 * 60 * 1000, // 48 horas
+      path: '/',
+    });
+
+    return res.json({
+      user: { id, name, email, role },
+      message: 'Authenticação realizada com sucesso!',
+    });
+  }
+
+  async delete(req, res){
+     res.clearCookie('token', { path: "/" });
+     return res.json({ message: 'Logout realizado com sucesso!' });
+
+  }
+}
+
+export default new AuthController();
