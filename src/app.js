58d69f9812b4a2d33ed7deff4eff2f0cbf3d2f7c
Lucas Alves <lucas1901alves@gmail.com>
2025-11-27 12:48:27 -0300

    config(cors): add Vite origin

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import helmet from 'helmet';
 import healthRoute from './routes/healthRoute.js';
 import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 class App {
   constructor() {
@@ -13,7 +14,9 @@ class App {
 
   middlewares() {
     this.server.use(cors({
-      origin: 'http://localhost:3000',
+      origin: ['http://localhost:3000',
+        'http://localhost:5173'
+      ],
       credentials: true,
     }));
     this.server.use(helmet());
@@ -23,6 +26,7 @@ class App {
   routes() {
     this.server.use(healthRoute);
     this.server.use('/auth', authRoutes);
+    this.server.use('/user', userRoutes);
   }
 }
 
