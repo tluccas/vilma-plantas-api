b1505d20ae4816486563ac45f5efd99237c45a34
Lucas Alves <lucas1901alves@gmail.com>
2025-11-29 21:27:27 -0300

    feat(route): add product route
    - add product route
    - add cookie parser

--- src/app.js
+++ src/app.js
@@ -4,11 +4,14 @@ import helmet from 'helmet';
 import healthRoute from './routes/healthRoute.js';
 import authRoutes from './routes/authRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import productRoutes from './routes/productRoutes.js';
+import cookieParser from 'cookie-parser';
 
 class App {
   constructor() {
     this.server = express();
     this.middlewares();
+    this.server.use(cookieParser());
     this.routes();
   }
 
@@ -27,6 +30,7 @@ class App {
     this.server.use(healthRoute);
     this.server.use('/auth', authRoutes);
     this.server.use('/user', userRoutes);
+    this.server.use('/product', productRoutes);
   }
 }
 
