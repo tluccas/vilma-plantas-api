0af6ad32817cf14d1540ce625eea077879f7c9b0
Lucas Alves <lucas1901alves@gmail.com>
2025-11-29 21:33:25 -0300

    feat(auth): add /user/profile:
    - GET :: user/profile/ to check user authentication

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import AuthController from '../controllers/auth/AuthController.js';
+import authMiddleware from '../middlewares/authMiddleware.js';
 
 const routes = new Router();
 
 routes.post('/login', AuthController.create);
 routes.post('/logout', AuthController.delete);
+routes.get('/profile', authMiddleware, AuthController.profile);
 
 export default routes;
