339bbde3550b5ee870c1dc5c91878de058d2b449
Lucas Alves <lucas1901alves@gmail.com>
2025-12-06 01:33:22 -0300

    feat(util): add UnauthorizedError & ForbiddenError exports

--- src/util/error/index.js
+++ src/util/error/index.js
@@ -2,3 +2,5 @@ export { default as AppError } from './AppError.js';
 export { default as NotFoundError } from './NotFoundError.js';
 export { default as ConflictError } from './ConflictError.js';
 export { default as ValidationError } from './ValidationError.js';
+export { default as UnauthorizedError } from './UnauthorizedError.js';
+export { default as ForbiddenError } from './ForbiddenError.js';
