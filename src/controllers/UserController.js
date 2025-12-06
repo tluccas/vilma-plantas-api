8d7d409be7f7a23a0226acd63f3f7d15124cc7ab
Lucas Alves <lucas1901alves@gmail.com>
2025-12-06 02:05:09 -0300

    refactor(controller): add error handling

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,42 +4,36 @@ import { createUserSchema } from '../validators/userValidator.js';
 const service = new UserService();
 
 class UserController {
-
-    async findAll(req, res) {
-        const users = await service.findAll();
-        return res.status(200).json(users);
+  async findAll(req, res, next) {
+    try {
+      const users = await service.findAll();
+      return res.status(200).json(users);
+    } catch (error) {
+      next(error);
     }
+  }
 
-    async findById(req, res) {
-        const { id } = req.params;
-        const user = await service.findById(id);
-        if (!user) {
-            return res.status(404).json({ message: 'Usuário não encontrado' });
-        }
-        return res.status(200).json(user);
-    
+  async findById(req, res, next) {
+    try {
+      const { id } = req.params;
+      const user = await service.findById(id);
+      return res.status(200).json(user);
+    } catch (error) {
+      next(error);
     }
+  }
 
-    async create(req, res) {
-        try {
-            await createUserSchema.validate(req.body, { abortEarly: false });
+  async create(req, res, next) {
+    try {
+      await createUserSchema.validate(req.body, { abortEarly: false });
 
-            const newUser = await service.create(req.body);
+      const newUser = await service.create(req.body);
 
-            return res.status(201).json(newUser);
-        } catch (error) {
-
-            if (error.name === 'ValidationError') {
-                return res.status(400).json({ message: error.errors });
-            }
-
-            if (error.status) {
-              return res.status(error.status).json({ message: error.message });
-            }
-            console.error('Erro no Controller ao criar usuário:', error);
-            return res.status(500).json({ message: 'Erro interno ao criar usuário.' });
-        }
+      return res.status(201).json(newUser);
+    } catch (error) {
+      next(error);
     }
+  }
 }
 
 export default new UserController();
